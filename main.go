e36fbf9d21631c8c19cb38ef3a93a2be81d3fc22
mikinano7 <halx25@gmail.com>
2016-05-30 21:02:46 +0900

    起動引数で指定したアカウントの設定を読み込めるよう修正

--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import (
 	"github.com/ChimeraCoder/anaconda"
 	"github.com/spf13/viper"
 	"log"
+	"flag"
 )
 
 func main() {
-	viper.SetConfigFile("./config.json")
+	flag.Parse()
+	account := flag.Args()[0]
 	viper.SetConfigType("json")
 	err := viper.ReadInConfig()
 	if err != nil {
@@ -18,9 +20,10 @@ func main() {
 	anaconda.SetConsumerKey(viper.GetString("twitter.consumer.key"))
 	anaconda.SetConsumerSecret(viper.GetString("twitter.consumer.secret"))
 
+	//TODO: アカウント設定とか追加して保持したい
 	api := anaconda.NewTwitterApi(
-		viper.GetString("twitter.credential.token.pn"),
-		viper.GetString("twitter.credential.secret.pn"),
+		viper.GetString("twitter.credential.token." + account),
+		viper.GetString("twitter.credential.secret." + account),
 	)
 
 	tw := &TweetWindow{api: api}
